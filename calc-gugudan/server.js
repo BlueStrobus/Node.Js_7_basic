8c2377d60e5084b38f1114f5e99d840020ce95ca
strobus <116132298+BlueStrobus@users.noreply.github.com>
2024-11-19 11:51:29 +0900

    문자를 숫자로 수정

--- calc-gugudan/server.js
+++ calc-gugudan/server.js
@@ -1,38 +1,34 @@
 import readlineSync from "readline-sync";
-
+// 설치 방법 : npm i 설치할것
 // 프로그램 시작
 function main() {
-  // 사용자로부터 입력을 받습니다.
-  const myChoice = readlineSync.question(
-    `1. 사칙연산 계산기   2. 구구단 출력기(2~9단)   3. 종료\n`
-  );
-  // console.log(`당신의 선택은: ${myChoice}`);
-  if (myChoice === "1") {
-    console.clear(); // 다 지움
-    console.log(`사칙연산 계산기를 선택하셨습니다.`);
-    const n = readlineSync.question(`첫번째 숫자를 적으세요\n`);
-    const f = readlineSync.question(`연산자를 적으세요\n`);
-    const m = readlineSync.question(`두번째 숫자를 적으세요\n`);
+  // 사용자로부터 입력ㅇ르 받습니다.
+  console.log(`1. 사칙연산 2. 구구단 출력 3. 종료`);
+  const myChoice = readlineSync.question("숫자를 입력해주세요 : ");
 
-    calculator(n, m, f);
-  } else if (myChoice === "2") {
-    console.clear(); // 다 지움
-    console.log(`구구단 출력기(2~9단)를 선택하셨습니다.`);
-    // 구구단 출력기 코드 추가
-    printMultiplicationTable();
-  } else if (myChoice === "3") {
-    console.log("종료합니다");
-  } else {
-    console.log("올바른 번호를 입력하세요.");
-    main();
+  switch (myChoice) {
+    case "1":
+      numInput();
+      break;
+    case "2":
+      printMultiplicationTable();
+      break;
+    case "3":
+      process.exit();
+    default:
+      console.log("잘못 입력하셨습니다.\n종료됩니다.");
   }
-  // 입력에 따라 해당 기능을 실행합니다.
-
-  // switch분기를 타서
-  // 1-> 사칙연산 계산기 실행
-  // 2-> 구구단 출력 프로그램 실행
 }
-// 사칙연산 계산기 함수
+
+function numInput() {
+  console.clear(); // 다 지움
+  console.log(`사칙연산 계산기를 선택하셨습니다.`);
+  const n = readlineSync.question(`첫번째 숫자를 적으세요\n`);
+  const f = readlineSync.question(`연산자를 적으세요(+ - / * %)\n`);
+  const m = readlineSync.question(`두번째 숫자를 적으세요\n`);
+  calculator(parseInt(n), parseInt(m), f);
+}
+
 function calculator(n, m, f) {
   // 여기에 코드를 작성하세요.
   if (f === "*") {
@@ -50,8 +46,9 @@ function calculator(n, m, f) {
   main();
 }
 
-// 구구단 출력 함수
 function printMultiplicationTable() {
+  console.clear(); // 다 지움
+  console.log(`구구단을 선택하셨습니다.`);
   // 여기에 코드를 작성하세요.
   for (let i = 2; i <= 9; i++) {
     console.log(`\n\n<<  ${i} 단 !  >>\n`);
@@ -64,5 +61,4 @@ function printMultiplicationTable() {
   main();
 }
 
-// 프로그램 실행
 main();
